e439908bab0bd131a5e7b53423fb626c503e47e7
Engjell Voca <vo.en0312@gmail.com>
2024-08-27 22:02:58 +0200

    Projektstruktur

---
+++ backend/EasyMoneyMapApi/src/main/java/com/example/easymoneymapapi/EasyMoneyMapApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.easymoneymapapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyMoneyMapApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyMoneyMapApiApplication.class, args);
+	}
+
+}
---
+++ frontend/android/app/src/main/java/com/example/easy_money_map_frontend/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.easy_money_map_frontend;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
